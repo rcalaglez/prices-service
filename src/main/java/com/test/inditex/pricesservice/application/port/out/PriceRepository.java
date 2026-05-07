27d6a69bae9536f756d916caf8ce237b50c5dff9
rcalaglez <rafacala7@gmail.com>
2026-05-07 20:15:17 +0100

    refactor: remove unused statements

--- src/main/java/com/test/inditex/pricesservice/application/port/out/PriceRepository.java
+++ src/main/java/com/test/inditex/pricesservice/application/port/out/PriceRepository.java
@@ -1,8 +1,6 @@
 package com.test.inditex.pricesservice.application.port.out;
 
-import com.test.inditex.pricesservice.domain.model.BrandId;
 import com.test.inditex.pricesservice.domain.model.Price;
-import com.test.inditex.pricesservice.domain.model.ProductId;
 
 import java.time.LocalDateTime;
 import java.util.List;
